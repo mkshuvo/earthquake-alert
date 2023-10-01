7f6d3570650962a87345bdbf158109ba1a807a13
Mohsin Khan Shuvo <mkshuvo123@gmail.com>
2023-10-01 23:59:09 +0600

    Adding intensity indicator with color

--- src/app/components/EarthquakeList.tsx
+++ src/app/components/EarthquakeList.tsx
@@ -1,116 +1,111 @@
 'use client'
+// Import React and Tailwind CSS
 import React, { useState, useEffect } from 'react';
 import GoogleLocation from './GoogleLocation';
 
 // Define an interface for the earthquake data
 interface Earthquake {
-    properties: {
-        mag: number;
-        place: string;
-        time: number;
-        updated: number;
-        tz: string | null;
-        url: string;
-        detail: string;
-        felt: number | null;
-        cdi: number | null;
-        mmi: number | null;
-        alert: string | null;
-        status: string;
-        tsunami: number;
-        sig: number;
-        net: string;
-        code: string;
-        ids: string;
-        sources: string;
-        types: string;
-        nst: number | null;
-        dmin: number | null;
-        rms: number;
-        gap: number | null;
-        magType: string;
-        type: string;
-        title: string;
-    };
-    geometry: {
-        type: string;
-        coordinates: [number,number,number];
-    };
-    id: string;
+  properties: {
+    mag: number;
+    place: string;
+    time: number;
+  };
+  geometry: {
+    type: string;
+    coordinates: [number, number, number];
+  };
+  id: string;
 }
-// interface Coordinates{
-//     lat: number,
-//     long: number, 
-//     alt: number
-// }
-
 
 const EarthquakeList: React.FC = () => {
-    const [earthquakes, setEarthquakes] = useState<Earthquake[]>([]);
+  const [earthquakes, setEarthquakes] = useState<Earthquake[]>([]);
 
-useEffect(() => {
-  const fetchEarthquakeData = () => {
-    fetch(
-      'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
-      // 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setEarthquakes(data.features.slice(0, 10));
-      })
-      .catch((error) => {
-        console.error('Error fetching earthquake data:', error);
-      });
-  };
-
-  fetchEarthquakeData();
-}, []);
-
-  
-    const getGoogleMapsLink = (lat: number, long: number) => {
-      return `https://www.google.com/maps?q=${lat},${long}`;
+  useEffect(() => {
+    const fetchEarthquakeData = () => {
+      fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson')
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setEarthquakes(data.features.slice(0, 10));
+        })
+        .catch((error) => {
+          console.error('Error fetching earthquake data:', error);
+        });
     };
 
-      
-  
-    return (
-      <div>
-        <h2>Earthquake List</h2>
-        <ul>
-          {earthquakes.map((earthquake, index) => (
-            <li key={index}>
-              <strong>Magnitude:</strong> {earthquake.properties.mag}<br />
-              <strong>Date and Time:</strong> {new Date(earthquake.properties.time).toLocaleString()}<br />
-              <strong>Coordinates:</strong> 
-              Latitude: {earthquake.geometry.coordinates[1]}, 
-              Longitude: {earthquake.geometry.coordinates[0]}, 
-              Altitude: {earthquake.geometry.coordinates[2]} km<br />
-              <strong>Location Name:</strong> 
-              <span id={`location-name-${index}`}>
-              <GoogleLocation lat={earthquake.geometry.coordinates[1]} long={earthquake.geometry.coordinates[0]} />
-              </span>
-              <br />
-              <strong>Google Maps:</strong> 
-              <a
-                href={getGoogleMapsLink(
-                  earthquake.geometry.coordinates[1],
-                  earthquake.geometry.coordinates[0]
-                )}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Open in Google Maps
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+    fetchEarthquakeData();
+  }, []);
+
+  const getGoogleMapsLink = (lat: number, long: number) => {
+    return `https://www.google.com/maps?q=${lat},${long}`;
   };
 
-  export default EarthquakeList;
+  const getColorIndicator = (magnitude: number) => {
+    if (magnitude >= 0 && magnitude < 3) {
+      return 'bg-green-500';
+    } else if (magnitude >= 3 && magnitude < 5) {
+      return 'bg-blue-500';
+    } else if (magnitude >= 5 && magnitude < 7) {
+      return 'bg-orange-500';
+    } else if (magnitude >= 7 && magnitude <= 9) {
+      return 'bg-red-800';
+    }
+    return ''; // Default color or no color
+  };
+
+  return (
+    <div className="container mx-auto mt-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {earthquakes.map((earthquake, index) => (
+          <div key={index} className="relative bg-white rounded-md overflow-hidden shadow-md">
+            {/* Color Indicator Sidebar */}
+            <div
+              className={`absolute top-0 bottom-0 left-0 ${
+                getColorIndicator(earthquake.properties.mag)
+              } w-3`}
+            ></div>
+            <div className="p-6">
+              <h1 className="text-2xl mb-2">
+                <p>Magnitude:</p> <strong className="text-3xl">{earthquake.properties.mag}</strong>
+              </h1>
+              <h3 className="text-xl font-semibold text-gray-800">{earthquake.properties.place}</h3>
+              <p className="text-gray-600 mb-2">
+                <strong>Date and Time:</strong>{' '}
+                {new Date(earthquake.properties.time).toLocaleString()}
+              </p>
+              <p className="text-gray-600 mb-2">
+                <strong>Coordinates:</strong> Latitude: {earthquake.geometry.coordinates[1]}, Longitude:{' '}
+                {earthquake.geometry.coordinates[0]}, Altitude: {earthquake.geometry.coordinates[2]} km
+              </p>
+              <p className="text-gray-600 mb-2">
+                <strong>Location Name:</strong>{' '}
+                <GoogleLocation lat={earthquake.geometry.coordinates[1]} long={earthquake.geometry.coordinates[0]} />
+              </p>
+              <p className="text-gray-600">
+                <strong>Google Maps:</strong>{' '}
+                <a
+                  href={getGoogleMapsLink(
+                    earthquake.geometry.coordinates[1],
+                    earthquake.geometry.coordinates[0]
+                  )}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:underline"
+                >
+                  Open in Google Maps
+                </a>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EarthquakeList;
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,15 @@
 // src/app/page.tsx
 import React from 'react';
-import DateFilter from './components/DateFilter';
-import MagnitudeFilter from './components/MagnitudeFilter';
-import LocationFilter from './components/LocationFilter';
 import EarthquakeList from './components/EarthquakeList';
 
 const Home: React.FC = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-semibold mb-4">Earthquake Alerts</h1>
-
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Earthquake List</h2>
       <div className="flex flex-wrap">
-        <div className="w-full md:w-1/4">
-          <DateFilter />
-          <MagnitudeFilter />
-          <LocationFilter />
-        </div>
-        <div className="w-full md:w-3/4">
-            <EarthquakeList />
-        </div>
+
+          <EarthquakeList />
+
       </div>
     </div>
   );
